ffbe9031a63bdbeff60087f9d57cc18f1c7c76eb
Diwei Guan <diweiguan@Diweis-MacBook-Pro.local>
2019-11-27 22:10:38 -0500

    removed whitenoise from settings to see if it's the problem

--- weather/settings.py
+++ weather/settings.py
@@ -42,7 +42,6 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -130,7 +129,7 @@ STATICFILES_DIRS = (
 )
 
 #  Add configuration for static files storage using whitenoise
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 
 import dj_database_url 
 prod_db  =  dj_database_url.config(conn_max_age=500)
